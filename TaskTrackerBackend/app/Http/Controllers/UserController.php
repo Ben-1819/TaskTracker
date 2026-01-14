aae93d91611965958999eb58c210d0142b2ad43e
Ben-1819 <ben.brown20332@gmail.com>
2026-01-14 20:12:20 +0000

    feat(function): Wrote index function

--- TaskTrackerBackend/app/Http/Controllers/UserController.php
+++ TaskTrackerBackend/app/Http/Controllers/UserController.php
@@ -13,7 +13,16 @@ class UserController extends Controller
      * index method:
      * Returns all methods from the users table
      */
-    public function index(){
+    public function index()
+    {
+        log::info('Index method in the user controller has been reached');
 
+        // Retrieve all records from the users table
+        $users = User::all();
+
+        log::info('All records from the users table have successfully been retrieved');
+
+        // Return as a Json response with status code 200 because this backend is being used as an API
+        return response()->json(["users" => $users], 200);
     }
 }
