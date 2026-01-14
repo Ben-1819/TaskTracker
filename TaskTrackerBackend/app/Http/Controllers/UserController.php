f1f1e70154d36df38463aab5ab23d4df7f7098e4
Ben-1819 <ben.brown20332@gmail.com>
2026-01-14 20:06:43 +0000

    feat(boilerplate): UserController boiletplate

--- TaskTrackerBackend/app/Http/Controllers/UserController.php
+++ TaskTrackerBackend/app/Http/Controllers/UserController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
+
 
 class UserController extends Controller
 {
-    //
+    /**
+     * index method:
+     * Returns all methods from the users table
+     */
+    public function index(){
+
+    }
 }
