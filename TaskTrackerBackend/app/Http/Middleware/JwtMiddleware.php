cfed3db165494e19156be2b4c173896ad1998d02
Ben-1819 <ben.brown20332@gmail.com>
2026-01-18 15:50:07 +0000

    feat(Middleware): Finished JWT Middleware

--- TaskTrackerBackend/app/Http/Middleware/JwtMiddleware.php
+++ TaskTrackerBackend/app/Http/Middleware/JwtMiddleware.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Log;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Exceptions\TokenExpiredException;
@@ -19,6 +20,19 @@ class JwtMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        // Try to parse the users JWT
+        try {
+            JWTAuth::parseToken()->authenticate();
+        } catch (TokenExpiredException $e) {
+            log::error('The token has expired');
+            return response()->json(['error' => 'The token has expired'], 401);
+        } catch (TokenInvalidException $e) {
+            log::error('The token is invalid');
+            return response()->json(['error' => 'The token is invalid'], 401);
+        } catch (JWTException $e) {
+            log::error('Token not found or malformed');
+            return response()->json(['error' => 'Token not found or malformed'], 401);
+        }
         return $next($request);
     }
 }
