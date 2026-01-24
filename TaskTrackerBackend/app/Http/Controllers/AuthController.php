c9a78835e3a0a9c613dec6fb8af5053c5e775c63
Ben-1819 <ben.brown20332@gmail.com>
2026-01-24 16:53:10 +0000

    feat(dependencies): Added dependencies

--- TaskTrackerBackend/app/Http/Controllers/AuthController.php
+++ TaskTrackerBackend/app/Http/Controllers/AuthController.php
@@ -3,6 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
 
 class AuthController extends Controller
 {
