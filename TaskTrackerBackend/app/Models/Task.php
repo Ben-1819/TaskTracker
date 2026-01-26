4aaf7f287a2033ebc815e997ecb216e89caa8f24
Ben-1819 <ben.brown20332@gmail.com>
2026-01-26 09:09:53 +0000

    feat(model): Made the task model

--- TaskTrackerBackend/app/Models/Task.php
+++ TaskTrackerBackend/app/Models/Task.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'category',
+        'date_set',
+        'date_due',
+        'complete',
+    ];
 }
