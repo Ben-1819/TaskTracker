a4f5d4268429bbac840f4c47935fc79e3065e652
Ben-1819 <ben.brown20332@gmail.com>
2026-02-04 12:57:06 +0000

    feat(routes): Added the routes to api.php

---
+++ TaskTrackerBackend/routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
+
+Route::contoller(AuthController::class)->group(function(){
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+});
+
+Route::controller(AuthController::class)->middleware('jwt')->group(function(){
+    Route::get('/user', 'getUser');
+    Route::post('/logout','logout');
+});
+
+Route::controller(UserController::class)->middleware('jwt')->group(function(){
+    Route::get('/userIndex', 'index');
+});
+
+Route::controller(TaskController::class)->middleware('jwt')->group(function(){
+    Route::get('/index', 'index');
+    Route::post('/store', 'store');
+});
+
+Route::controller(TaskController::class)->middleware(['jwt', 'taskOwner'])->group(function(){
+    Route::get("/{id}/show", 'show');
+    Route::put("/{id}/update", 'update');
+    Route::put("/{id}/complete", 'complete');
+    Route::delete("/{id}/delete", 'destroy');
+});
