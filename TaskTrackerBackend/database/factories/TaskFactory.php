e6a8e03eb3ef725bfa2d5e2912f470cc31abde84
Ben-1819 <ben.brown20332@gmail.com>
2026-02-13 12:26:54 +0000

    fix: Removed extra comma in the task factory

--- TaskTrackerBackend/database/factories/TaskFactory.php
+++ TaskTrackerBackend/database/factories/TaskFactory.php
@@ -19,7 +19,7 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-            'user_id' => User::factory(),,
+            'user_id' => User::factory(),
             'name' => $this->faker->name(),
             'description' => $this->faker->words(10, true),
             'category' => $this->faker->words(1, true),
