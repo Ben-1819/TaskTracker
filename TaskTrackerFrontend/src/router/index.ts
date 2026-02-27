39f534f79297b44e56681d401e08e5dccae8b6be
Ben-1819 <ben.brown20332@gmail.com>
2026-02-27 08:34:22 +0000

    feat: Added the sign in view to the router

--- TaskTrackerFrontend/src/router/index.ts
+++ TaskTrackerFrontend/src/router/index.ts
@@ -1,14 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import SignIn from '@/views/SignIn.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    /*{
-      path: '/',
-      name: '',
-      component: ,
-    },*/
+    {
+      path: '/login',
+      name: 'SignIn',
+      component: SignIn,
+    },
   ],
-})
+});
 
-export default router
+export default router;
