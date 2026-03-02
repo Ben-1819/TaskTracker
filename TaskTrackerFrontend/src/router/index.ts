e0c48eb86ecd3269670eaa93131ae847b5e55329
Ben-1819 <ben.brown20332@gmail.com>
2026-03-02 13:31:29 +0000

    feat: Added the register view to router

--- TaskTrackerFrontend/src/router/index.ts
+++ TaskTrackerFrontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import SignIn from '@/views/SignIn.vue';
+import Register from '@/views/Register.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/login',
       name: 'SignIn',
       component: SignIn,
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register,
     },
   ],
 });
