18e61a7c5cad5e3a1e4946ba14e0bd5b3376329e
Ben-1819 <ben.brown20332@gmail.com>
2026-04-09 10:11:36 +0100

    feat: Installed shadcn-vue dropdown
    Installed the shadcn-vue dropdown and created a task category dropdown
    using it

---
+++ TaskTrackerFrontend/src/components/TaskCategoryDropdown.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+const props = defineProps<{
+  modelValue: string | null;
+}>();
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void;
+}>();
+
+const taskCategories: string[] = ['Housework', 'Study', 'Family', 'Sports'];
+
+const selectCategory = (category: string) => {
+  emit('update:modelValue', category);
+};
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger>{{ props.modelValue || 'Select Category' }}</DropdownMenuTrigger>
+    <DropdownMenuContent>
+      <DropdownMenuLabel>Select a task category</DropdownMenuLabel>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem v-for="item in taskCategories" :key="item" @click="selectCategory(item)">{{
+        item
+      }}</DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
