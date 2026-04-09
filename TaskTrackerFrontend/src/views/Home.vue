edb28387b7e96ba9ff7165ef580aa07b04c0f95f
Ben-1819 <ben.brown20332@gmail.com>
2026-04-09 09:07:31 +0100

    feat: Created the Home view
    Created the home view and laid out the methods that will be used in it,
    navigation button were laid out and the routes were predefined and
    commented out

--- TaskTrackerFrontend/src/views/Home.vue
+++ TaskTrackerFrontend/src/views/Home.vue
@@ -2,7 +2,7 @@
 import { useAuthStore } from '@/stores/auth';
 import { useRouter } from 'vue-router';
 import { onMounted, reactive } from 'vue';
-import { Button } from '@/components/ui/button';
+import NavigationButton from '@/components/NavigationButton.vue';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import Sidebar from '@/components/Sidebar.vue';
 
@@ -43,6 +43,36 @@ const goToSignIn = (): void => {
     name: 'SignIn',
   });
 };
+
+const goToCreateTask = (): void => {
+  // router.push({
+  //   name: 'CreateTask'
+  // });
+};
+
+const goToViewTasks = (): void => {
+  // router.push({
+  //   name: 'TaskIndex'
+  // });
+};
+
+const goToCurrentTasks = (): void => {
+  // router.push({
+  //   name: 'CurrentTasks'
+  // });
+};
+
+const goToIncompleteTasks = (): void => {
+  // router.push({
+  //   name: 'IncompleteTasks'
+  // });
+};
+
+const goToCompletedTasks = (): void => {
+  // router.push({
+  //   name: 'CompletedTasks'
+  // });
+};
 </script>
 
 <template>
@@ -53,7 +83,16 @@ const goToSignIn = (): void => {
         <SidebarTrigger />
       </div>
       <div class="flex justify-center">
-        <h1 class="text-xl">Home</h1>
+        <h1 class="text-xl pb-10">Welcome to Task Tracker</h1>
+      </div>
+      <div class="flex justify-center">
+        <div class="grid grid-cols-2 gap-8">
+          <NavigationButton @click="goToCreateTask">Create task</NavigationButton>
+          <NavigationButton @click="goToViewTasks">View tasks</NavigationButton>
+          <NavigationButton @click="goToCurrentTasks">Current Tasks</NavigationButton>
+          <NavigationButton @click="goToIncompleteTasks">Incomplete Tasks</NavigationButton>
+          <NavigationButton @click="goToCompletedTasks">Completed Tasks</NavigationButton>
+        </div>
       </div>
     </main>
   </SidebarProvider>
