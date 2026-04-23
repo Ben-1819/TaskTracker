cbfbbf06223a780dbe962207b3b51c243638d3b6
Ben-1819 <ben.brown20332@gmail.com>
2026-04-23 12:37:46 +0100

    feat: Created task views and added to router

--- TaskTrackerFrontend/src/router/index.ts
+++ TaskTrackerFrontend/src/router/index.ts
@@ -4,6 +4,9 @@ import SignIn from '@/views/SignIn.vue';
 import Register from '@/views/Register.vue';
 import Create from '@/views/Task/Create.vue';
 import Index from '@/views/Task/Index.vue';
+import Current from '@/views/Task/Current.vue';
+import Incomplete from '@/views/Task/Incomplete.vue';
+import Completed from '@/views/Task/Completed.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +35,21 @@ const router = createRouter({
       path: '/task/index',
       name: 'Tasks',
       component: Index,
+    },
+    {
+      path: '/task/current',
+      name: 'CurrentTasks',
+      component: Current,
+    },
+    {
+      path: '/task/incomplete',
+      name: 'IncompleteTasks',
+      component: Incomplete,
+    },
+    {
+      path: '/task/completed',
+      name: 'CompletedTasks',
+      component: Completed,
     },
   ],
 });
---
+++ TaskTrackerFrontend/src/views/Task/Completed.vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+import { reactive, onMounted, ref } from 'vue';
+import { useAuthStore } from '@/stores/auth';
+import { useRouter } from 'vue-router';
+import Sidebar from '@/components/Sidebar.vue';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import axios from 'axios';
+import TaskCard from '@/components/TaskCard.vue';
+
+type Token = string | null;
+const authStore = useAuthStore();
+const router = useRouter();
+
+interface User {
+  firstName: string | undefined;
+  lastName: string | undefined;
+}
+
+interface TaskAPI {
+  id: number;
+  userId: number;
+  name: string;
+  description: string;
+  category: string;
+  date_set: Date;
+  date_due: Date;
+  completed: boolean;
+}
+
+interface Task {
+  id: number;
+  userId: number;
+  name: string;
+  description: string;
+  category: string;
+  dateSet: Date;
+  dateDue: Date;
+  completed: boolean;
+}
+
+const user = reactive<User>({
+  firstName: '',
+  lastName: '',
+});
+
+const tasks = ref<Task[]>([]);
+
+onMounted(async () => {
+  const token: Token = localStorage.getItem('token');
+  if (token) {
+    authStore.setToken(token);
+    await authStore.fetchUser();
+    user.firstName = authStore.user?.first_name;
+    user.lastName = authStore.user?.last_name;
+    getCompletedTasks();
+  } else if (token === null) {
+    logout();
+  }
+});
+
+const logout = (): void => {
+  authStore.logout();
+  goToSignIn();
+};
+
+const goToSignIn = (): void => {
+  router.push({
+    name: 'SignIn',
+  });
+};
+
+const getCompletedTasks = async (): Promise<void> => {
+  try {
+    const response = await axios.get('http://127.0.0.1:8000/api/completed', {
+      headers: {
+        Authorization: `Bearer ${authStore.token}`,
+      },
+    });
+    console.log('Completed tasks retrieved');
+
+    tasks.value = response.data.completed_tasks.map((task: TaskAPI) => ({
+      id: task.id,
+      userId: task.userId,
+      name: task.name,
+      description: task.description,
+      category: task.category,
+      dateSet: new Date(task.date_set),
+      dateDue: new Date(task.date_due),
+      completed: task.completed,
+    }));
+  } catch (error: unknown) {
+    console.error(
+      `[FETCH-COMPLETED-ERROR] An error occurred while fetching completed tasks: ${error}`,
+    );
+  }
+};
+</script>
+
+<template>
+  <SidebarProvider>
+    <Sidebar :first-name="user.firstName" :last-name="user.lastName" @logout-event="logout()" />
+    <main class="w-full">
+      <div class="flex justify-start">
+        <SidebarTrigger />
+      </div>
+      <h1 class="text-2xl text-center">Completed Tasks</h1>
+      <div v-if="tasks.length === 0" class="mt-20">
+        <h2 class="text-xl text-center text-green-600">You have no completed tasks.</h2>
+      </div>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <TaskCard v-for="task in tasks" :key="task.id" :task="task" />
+      </div>
+    </main>
+  </SidebarProvider>
+</template>
