b2a2cceaca02e058772130698afad6fce04edc9c
Ben-1819 <ben.brown20332@gmail.com>
2026-04-23 11:30:15 +0100

    feat: Added routes for additional task endpoints

--- TaskTrackerBackend/routes/api.php
+++ TaskTrackerBackend/routes/api.php
@@ -6,26 +6,29 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 
-Route::controller(AuthController::class)->group(function(){
+Route::controller(AuthController::class)->group(function () {
     Route::post('/register', 'register');
     Route::post('/login', 'login');
 });
 
-Route::controller(AuthController::class)->middleware('jwt')->group(function(){
+Route::controller(AuthController::class)->middleware('jwt')->group(function () {
     Route::get('/user', 'getUser');
-    Route::post('/logout','logout');
+    Route::post('/logout', 'logout');
 });
 
-Route::controller(UserController::class)->middleware('jwt')->group(function(){
+Route::controller(UserController::class)->middleware('jwt')->group(function () {
     Route::get('/userIndex', 'index');
 });
 
-Route::controller(TaskController::class)->middleware('jwt')->group(function(){
+Route::controller(TaskController::class)->middleware('jwt')->group(function () {
     Route::get('/index', 'index');
+    Route::get('/completed', 'completedTasks');
+    Route::get('/incomplete', 'incompleteTasks');
+    Route::get('/current', 'currentTasks');
     Route::post('/store', 'store');
 });
 
-Route::controller(TaskController::class)->middleware(['jwt', 'taskOwner'])->group(function(){
+Route::controller(TaskController::class)->middleware(['jwt', 'taskOwner'])->group(function () {
     Route::get("/{id}/show", 'show');
     Route::put("/{id}/update", 'update');
     Route::put("/{id}/complete", 'complete');
